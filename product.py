af0ecad34246d0f81ab3652b6890b5c3ff9e4391
Carlos G?lvez <carlos.galvez@nan-tic.com>
2019-06-06 09:58:57 +0200

    Mark if package is default. #037520

--- product.py
+++ product.py
@@ -4,6 +4,8 @@ from trytond.model import ModelView, ModelSQL, Check, fields
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
 from trytond.transaction import Transaction
+from trytond.i18n import gettext
+from trytond.exceptions import UserError
 
 from trytond.modules.product.product import STATES, DEPENDS
 
@@ -22,6 +24,7 @@ class Package(ModelSQL, ModelView):
     quantity = fields.Float('Quantity', required=True,
         domain=[('quantity', '>', 0)], digits=(16, Eval('unit_digits', 2)),
         depends=['unit_digits'])
+    is_default = fields.Boolean('Default')
 
     @classmethod
     def __setup__(cls):
@@ -29,18 +32,8 @@ class Package(ModelSQL, ModelView):
         t = cls.__table__()
         cls._sql_constraints += [
             ('quantity_greater_than_zero', Check(t, t.quantity > 0),
-                'Quantity per package must be greater than zero.'),
+                'product_package.msg_quantity_greater_than_zero'),
             ]
-
-    @fields.depends('product')
-    def on_change_with_unit_digits(self, name=None):
-        if self.product:
-            return self.product.default_uom.digits
-        return 2
-
-    @staticmethod
-    def default_quantity():
-        return 1
 
     @staticmethod
     def default_unit_digits():
@@ -51,6 +44,40 @@ class Package(ModelSQL, ModelView):
             uom = Uom(uom_id)
             return uom.digits
         return 1
+
+    @staticmethod
+    def default_quantity():
+        return 1
+
+    @staticmethod
+    def default_is_default():
+        return True
+
+    @fields.depends('product')
+    def on_change_with_unit_digits(self, name=None):
+        if self.product:
+            return self.product.default_uom.digits
+        return 2
+
+    @classmethod
+    def validate(cls, packages):
+        super(Package, cls).validate(packages)
+
+        products = []
+        is_unique = True
+        for package in packages:
+            if package.is_default:
+                for pack in package.product.packages:
+                    if pack.is_default:
+                        product_id = pack.product.id
+                        if product_id in products:
+                            is_unique = False
+                            break
+                        products.append(product_id)
+
+        if not is_unique:
+            raise UserError(gettext(
+                'product_package.msg_is_default_unique'))
 
 
 class Template(metaclass=PoolMeta):
