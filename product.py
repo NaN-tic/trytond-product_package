f72572129b8d564a77fa4e86338f6c0fe7144489
Raimon Esteve <raimon@nan-tic.com>
2019-06-21 14:46:31 +0200

    Not allow change quantity package when have current packages + Not create a package when that already use the product without packages #038038

--- product.py
+++ product.py
@@ -1,14 +1,63 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+import functools
 from trytond.model import ModelView, ModelSQL, Check, fields
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval, Bool
 from trytond.transaction import Transaction
 from trytond.i18n import gettext
 from trytond.exceptions import UserError
+from trytond.model.exceptions import AccessError
+from trytond.tools import grouped_slice
 
 
 __all__ = ['Package', 'Template']
+
+def check_new_package(func):
+
+    def find_packages(cls, model, products):
+        Model = Pool().get(model)
+        for sub_records in grouped_slice(products):
+            rows = Model.search([
+                    ('product', 'in', list(map(int, sub_records))),
+                    ('product_package', '=', None),
+                    ],
+                limit=1, order=[])
+            if rows:
+                return rows
+        return False
+
+    @functools.wraps(func)
+    def decorator(cls, vlist):
+        Package =  Pool().get('product.package')
+
+        transaction = Transaction()
+        if (transaction.user != 0 and transaction.context.get('_check_access')):
+            products = list(set([r.get('product') for r in vlist]))
+            for model, msg in Package._create_package:
+                if find_packages(cls, model, products):
+                    raise AccessError(gettext(msg))
+        func(cls, vlist)
+    return decorator
+
+def check_no_package(func):
+
+    @functools.wraps(func)
+    def decorator(cls, *args):
+        Package =  Pool().get('product.package')
+
+        transaction = Transaction()
+        if (transaction.user != 0 and transaction.context.get('_check_access')):
+            actions = iter(args)
+            for records, values in zip(actions, actions):
+                for field, msg in Package._modify_no_package:
+                    if field in values:
+                        if Package.find_packages(records):
+                            raise AccessError(gettext(msg))
+                        # No packages for those records
+                        break
+        func(cls, *args)
+    return decorator
 
 
 class Package(ModelSQL, ModelView):
@@ -33,6 +82,10 @@ class Package(ModelSQL, ModelView):
             ('quantity_greater_than_zero', Check(t, t.quantity > 0),
                 'product_package.msg_quantity_greater_than_zero'),
             ]
+        cls._modify_no_package = [
+            ('quantity', 'product_package.msg_product_package_qty'),
+            ]
+        cls._create_package = []
 
     @staticmethod
     def default_unit_digits():
@@ -77,6 +130,20 @@ class Package(ModelSQL, ModelView):
         if not is_unique:
             raise UserError(gettext(
                 'product_package.msg_is_default_unique'))
+
+    @classmethod
+    @check_new_package
+    def create(cls, vlist):
+        return super(Package, cls).create(vlist)
+
+    @classmethod
+    @check_no_package
+    def write(cls, *args):
+        super(Package, cls).write(*args)
+
+    @classmethod
+    def find_packages(cls, records):
+        return False
 
 
 class Template(metaclass=PoolMeta):
